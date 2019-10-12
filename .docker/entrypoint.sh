426ad30d879380950d0baa618c704f0ad6e92b00
joao.silva <joao.silva@arapiraca.al.gov.br>
2019-10-11 22:16:47 -0300

    Vue Components do Laravel Passport e requisição de token

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 #On error no such file entrypoint.sh, execute in terminal - dos2unix .docker\entrypoint.sh
-
 cp .env.example .env
 cp .env.testing.example .env.testing
 composer install
 php artisan key:generate
 php artisan migrate --seed
 php artisan passport:install
+npm install && npm run dev
 chown -R www-data:1000 /var/www/storage /var/www/public /var/www/app
 chmod -R 775 /var/www/storage /var/www/public /var/www/app
 chmod -R 777 /var/www/bootstrap/cache
